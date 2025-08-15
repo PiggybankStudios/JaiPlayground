b94aebd64d1b1c266541c5eb81d57fcd435147be
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2025-08-14 20:07:42 -0700

    Initial commit

---
+++ build.bat
@@ -0,0 +1,6 @@
+@echo off
+REM By default blocks like IF and FOR in batch are treated as a single command and variable values are expended when the block is entered
+REM Enabling "delayed expansion" allows us to use !variable! syntax inside blocks to get the value of variables on that line (and at each iteration in the case of loops)
+setlocal enabledelayedexpansion
+
+jai build.jai
